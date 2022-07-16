ab03473d511d5c45a2f094799f5e7ffa753eb981
wangbaocong <972048442@qq.com>
2022-07-16 22:48:14 +0800

    edit

--- test_04.py
+++ test_04.py
@@ -1,15 +1,15 @@
 import random
 
 list = ["a","b","c","d","A","e","B","f"]
-# for index,item in enumerate(list):
-   # print(index+1,item)
-   #  if index % 2 == 0:
-   #      print(item+"\t\t",end="")
-   #  else:
-   #      print(item+"\n")
-print(list.count("a"))
-list.sort(key=str.lower)
-print(list)
-
-list2 = [random.randint(20,40) for i in range(34)]
-print( list2)
+for index,item in enumerate(list):
+    print(index+1,item)
+    if index % 2 == 0:
+        print(item+"\t\t",end="")
+    else:
+        print(item+"\n")
+# print(list.count("a"))
+# list.sort(key=str.lower)
+# print(list)
+#
+# list2 = [random.randint(20,40) for i in range(34)]
+# print( list2)
